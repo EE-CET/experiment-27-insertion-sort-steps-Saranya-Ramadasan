eaad8dddb8faaa89afac894e718fc9536e1d3378
Saranya-Ramadasan <rsnyaramadasan04@gmail.com>
2026-04-08 16:48:54 +0530

    1st commit

--- src/InsertionSortSteps.java
+++ src/InsertionSortSteps.java
@@ -1,16 +1,37 @@
 import java.util.Scanner;
 
 public class InsertionSortSteps {
-
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        if (!sc.hasNextInt()) return;
+        
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+        
+        int key = arr[n - 1];
+        int i = n - 2;
+        
+        while (i >= 0 && arr[i] > key) {
+            arr[i + 1] = arr[i];
+            printArray(arr);
+            i--;
+        }
+        
+        arr[i + 1] = key;
+        printArray(arr);
+        
+        sc.close();
+    }
     
-        // TODO: Read n
-        // TODO: Read the array elements
-
-        // TODO: Implement the insertion logic:
-        // 1. Store the last element (key)
-        // 2. Iterate backwards from the second-to-last element
-        // 3. If current element > key, shift it right and print array
-        // 4. If current element <= key, insert key and print array (then break)
-        // Note: Don't forget the case where key is the smallest element (index 0)
-    
+    private static void printArray(int[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + (i == arr.length - 1 ? "" : " "));
+        }
+        System.out.println();
+    }
 }
